b31bb8e4f3dda641c0bbbf117b37190df3e0a0ae
Joseph LaVia <josephdlavia@gmail.com>
2023-05-22 11:05:05 -0500

    class exercise

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var myCar = new Car();
+            myCar.Make = "honda";
+            myCar.Model = "civic";
+            myCar.Year = "2003";
+
+            Console.WriteLine($"This car is a {myCar.Make} and the model is a {myCar.Model} and it is from {myCar.Year}");
+
         }
     }
 }
