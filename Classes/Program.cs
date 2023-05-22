9653101df3169f5f2616e3737823c1b43897621a
Joseph LaVia <josephdlavia@gmail.com>
2023-05-22 13:12:27 -0500

    class exercise

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -7,7 +7,7 @@
             var myCar = new Car();
             myCar.Make = "honda";
             myCar.Model = "civic";
-            myCar.Year = "2003";
+            myCar.Year = 2003;
 
             Console.WriteLine($"This car is a {myCar.Make} and the model is a {myCar.Model} and it is from {myCar.Year}");
 
